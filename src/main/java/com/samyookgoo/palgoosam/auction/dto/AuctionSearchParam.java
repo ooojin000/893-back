1fd763a9d5c9f8c3ba9d95e5b71ec88009543e46
CodeVac513 <xssdsw@naver.com>
2025-05-07 13:58:56 +0900

    feat: 검색에 사용되는 DTO, DB 검색 결과를 반환하는 DTO, 클라이언트의 요청에 대한 응답 DTO 구현

---
+++ src/main/java/com/samyookgoo/palgoosam/auction/dto/AuctionSearchParam.java
@@ -0,0 +1,39 @@
+package com.samyookgoo.palgoosam.auction.dto;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuctionSearchParam {
+    private String keyword;
+
+    private Integer categoryId;
+
+    // 상품 사용감
+    private Boolean isBrandNew;
+    private Boolean isLikeNew;
+    private Boolean isLightlyUsed;
+    private Boolean isHeavilyUsed;
+    private Boolean isDefective;
+
+    private Integer minPrice;
+    private Integer maxPrice;
+
+    private Boolean isPending;
+    private Boolean isActive;
+    private Boolean isCompleted;
+
+    private Integer page = 1;
+    private Integer limit = 10;
+
+    @NotNull
+    private String sortBy = "latest";
+}
