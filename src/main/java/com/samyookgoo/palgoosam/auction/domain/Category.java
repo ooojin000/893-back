c7bdfb2e3d4b536b15c80e7ec3b325224919d087
김우진 <ooojin523@gmail.com>
2025-05-07 17:00:45 +0900

    feat: Getter 어노테이션 추가

--- src/main/java/com/samyookgoo/palgoosam/auction/domain/Category.java
+++ src/main/java/com/samyookgoo/palgoosam/auction/domain/Category.java
@@ -12,7 +12,9 @@ import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import java.util.ArrayList;
 import java.util.List;
+import lombok.Getter;
 
+@Getter
 @Entity
 @Table(name = "category")
 public class Category {
