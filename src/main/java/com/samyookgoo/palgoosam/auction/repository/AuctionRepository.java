ff80e78280b832eea9ccdddccac02f6d22f1139d
김우진 <ooojin523@gmail.com>
2025-05-07 17:20:43 +0900

    feat: 경매 상품 카테고리 및 판매자 조회 repository

--- src/main/java/com/samyookgoo/palgoosam/auction/repository/AuctionRepository.java
+++ src/main/java/com/samyookgoo/palgoosam/auction/repository/AuctionRepository.java
@@ -3,9 +3,18 @@ package com.samyookgoo.palgoosam.auction.repository;
 import com.samyookgoo.palgoosam.auction.domain.Auction;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AuctionRepository extends JpaRepository<Auction, Long> {
     Optional<Auction> findById(Long id);
+
+    @Query("SELECT a FROM Auction a " +
+            "JOIN FETCH a.category " +
+            "JOIN FETCH a.seller " +
+            "WHERE a.id = :auctionId")
+    Optional<Auction> findByIdWithCategoryAndSeller(@Param("auctionId") Long auctionId);
+
 }
