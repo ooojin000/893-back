bfba13f22987d5537d33083014e3112b6510d772
김우진 <ooojin523@gmail.com>
2025-05-07 17:22:44 +0900

    feat: 대표 이미지 조회를 위한 Query문

--- src/main/java/com/samyookgoo/palgoosam/auction/repository/AuctionImageRepository.java
+++ src/main/java/com/samyookgoo/palgoosam/auction/repository/AuctionImageRepository.java
@@ -2,10 +2,18 @@ package com.samyookgoo.palgoosam.auction.repository;
 
 import com.samyookgoo.palgoosam.auction.domain.AuctionImage;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AuctionImageRepository extends JpaRepository<AuctionImage, Long> {
     List<AuctionImage> findByAuctionId(Long auctionId);
+
+    @Query("SELECT a FROM AuctionImage a " +
+            "WHERE a.auction.id = :auctionId AND a.imageSeq = 0")
+    Optional<AuctionImage> findMainImageByAuctionId(@Param("auctionId") Long auctionId);
+
 }
