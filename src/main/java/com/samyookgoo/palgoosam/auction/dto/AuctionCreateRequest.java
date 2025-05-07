3d8498163ec9978e19eb9e78f0bae8d43136a2bd
김우진 <ooojin523@gmail.com>
2025-05-07 17:10:06 +0900

    fix: mainImageIndex 변수 삭제

--- src/main/java/com/samyookgoo/palgoosam/auction/dto/AuctionCreateRequest.java
+++ src/main/java/com/samyookgoo/palgoosam/auction/dto/AuctionCreateRequest.java
@@ -13,5 +13,4 @@ public class AuctionCreateRequest {
     private int startDelay;
     private int durationTime;
     private Long categoryId;
-    private Integer mainImageIndex; // 대표 이미지 인덱스 (프론트에서 클릭한 이미지가 대표 이미지로 설정됨 / 인덱스 0 ~ 9)
 }
