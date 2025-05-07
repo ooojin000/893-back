30cdc8fbaf06e18e3d7aadbebc27487dbf764247
CodeVac513 <xssdsw@naver.com>
2025-05-07 14:02:22 +0900

    feat: imageRepository에서 auctionId로 썸네일을 찾을 수 있도록 메서드 구현

---
+++ src/main/java/com/samyookgoo/palgoosam/auction/repository/ImageRepository.java
@@ -0,0 +1,19 @@
+package com.samyookgoo.palgoosam.auction.repository;
+
+import com.samyookgoo.palgoosam.auction.domain.AuctionImage;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ImageRepository extends JpaRepository<AuctionImage, Long> {
+    @Query(value = """
+            SELECT i.*
+            FROM auction_image i
+            WHERE i.auction_id IN (:auctionIdList) AND i.is_main = true
+            """, nativeQuery = true)
+    List<AuctionImage> findThumbnailsByAuctionIds(@Param("auctionIdList") List<Long> auctionIdList);
+
+}
