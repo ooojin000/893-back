1556a70a3359c65d900a0060d59dd5231f7ca02b
jiyeonyh <wldus8827@gmail.com>
2025-05-08 14:52:44 +0900

    feat: add validation to BidRequest(not null, min 0)

--- src/main/java/com/samyookgoo/palgoosam/bid/controller/request/BidRequest.java
+++ src/main/java/com/samyookgoo/palgoosam/bid/controller/request/BidRequest.java
@@ -1,8 +1,12 @@
 package com.samyookgoo.palgoosam.bid.controller.request;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class BidRequest {
+    @NotNull(message = "입찰 금액은 필수입니다.")
+    @Min(value = 0, message = "입찰 금액은 0원 이상이어야 합니다.")
     private Integer price;
 }
