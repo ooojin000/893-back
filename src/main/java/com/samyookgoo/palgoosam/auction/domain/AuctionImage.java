7f49c74af026c72c8687ba52a377fd5212cceb12
CodeVac513 <xssdsw@naver.com>
2025-05-09 12:43:37 +0900

    feat: auctionImage 엔티티의 생성자 추가

--- src/main/java/com/samyookgoo/palgoosam/auction/domain/AuctionImage.java
+++ src/main/java/com/samyookgoo/palgoosam/auction/domain/AuctionImage.java
@@ -10,8 +10,10 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
@@ -21,6 +23,8 @@ import org.hibernate.annotations.CreationTimestamp;
 @Builder
 @Entity
 @Table(name = "auction_image")
+@AllArgsConstructor
+@NoArgsConstructor
 public class AuctionImage {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
