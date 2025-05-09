656a9ed32ed0b39e7abf3d2da9eae7ba93b70471
CodeVac513 <xssdsw@naver.com>
2025-05-09 18:38:31 +0900

    feat: 검색 기록 repository 구현

---
+++ src/main/java/com/samyookgoo/palgoosam/search/repository/SearchHistoryRepository.java
@@ -0,0 +1,21 @@
+package com.samyookgoo.palgoosam.search.repository;
+
+import com.samyookgoo.palgoosam.search.domain.SearchHistory;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SearchHistoryRepository extends JpaRepository<SearchHistory, Long> {
+
+    @Query(value = """
+            SELECT *
+            FROM search_history s
+            WHERE user_id = :userId AND is_deleted = false
+            ORDER BY s.created_at
+            LIMIT 10
+            """, nativeQuery = true)
+    List<SearchHistory> findAllByUserId(@Param("userId") Long userId);
+}
