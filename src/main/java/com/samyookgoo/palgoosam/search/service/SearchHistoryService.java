253ad1522af0ffdb31003936e98fbe28ed178855
CodeVac513 <xssdsw@naver.com>
2025-05-09 18:39:29 +0900

    feat: 검색 기록 조회 메서드 구현

---
+++ src/main/java/com/samyookgoo/palgoosam/search/service/SearchHistoryService.java
@@ -0,0 +1,36 @@
+package com.samyookgoo.palgoosam.search.service;
+
+
+import com.samyookgoo.palgoosam.search.domain.SearchHistory;
+import com.samyookgoo.palgoosam.search.dto.SearchHistoryResponseDto;
+import com.samyookgoo.palgoosam.search.repository.SearchHistoryRepository;
+import com.samyookgoo.palgoosam.user.domain.User;
+import com.samyookgoo.palgoosam.user.repository.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class SearchHistoryService {
+    private final SearchHistoryRepository searchHistoryRepository;
+    private final UserRepository userRepository;
+
+    public List<SearchHistoryResponseDto> getSearchHistory() {
+        /*
+        userService에서 사용자 판별하는 부분 추가
+        if (user != null) 아래 로직 동작하도록
+         */
+
+        User dummyUser = userRepository.findById(1L).orElseThrow(() -> new EntityNotFoundException("User not found"));
+        List<SearchHistory> searchHistoryList = searchHistoryRepository.findAllByUserId(dummyUser.getId());
+        return searchHistoryList.stream().map(searchHistory ->
+                SearchHistoryResponseDto.builder().id(searchHistory.getId()).keyword(searchHistory.getKeyword())
+                        .createdAt(searchHistory.getCreatedAt()).updatedAt(searchHistory.getUpdatedAt())
+                        .build()).toList();
+    }
+
+}
