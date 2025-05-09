e533e01f43af98b4ee7f361defbe0e288dbb6fcf
CodeVac513 <xssdsw@naver.com>
2025-05-09 17:34:41 +0900

    feat: 검색어 저장 API 호출 시 사용할 request dto 구현

---
+++ src/main/java/com/samyookgoo/palgoosam/auction/dto/SearchHistoryCreateRequestDto.java
@@ -0,0 +1,10 @@
+package com.samyookgoo.palgoosam.auction.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SearchHistoryCreateRequestDto {
+    String keyword;
+}
