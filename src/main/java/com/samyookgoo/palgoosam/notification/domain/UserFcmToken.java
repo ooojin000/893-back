9032224a1f101291c0c8e2248694af66c17b3dd6
CodeVac513 <xssdsw@naver.com>
2025-05-10 18:50:59 +0900

    feat: user_fcm_token entity 경로 및 속성 수정

---
+++ src/main/java/com/samyookgoo/palgoosam/notification/domain/UserFcmToken.java
@@ -0,0 +1,49 @@
+package com.samyookgoo.palgoosam.notification.domain;
+
+import com.samyookgoo.palgoosam.user.domain.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "user_fcm_token")
+public class UserFcmToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(nullable = false)
+    private String token;
+
+    @NotNull
+    private String deviceType = "WEB";
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
