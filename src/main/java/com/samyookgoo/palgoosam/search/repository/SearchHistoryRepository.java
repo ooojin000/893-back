8da272378a7a08cadb0296ef904993460b3e4e69
CodeVac513 <xssdsw@naver.com>
2025-05-12 11:15:44 +0900

    refactor: repository 어노테이션 삭제

--- src/main/java/com/samyookgoo/palgoosam/search/repository/SearchHistoryRepository.java
+++ src/main/java/com/samyookgoo/palgoosam/search/repository/SearchHistoryRepository.java
@@ -4,9 +4,7 @@ import com.samyookgoo.palgoosam.search.domain.SearchHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface SearchHistoryRepository extends JpaRepository<SearchHistory, Long> {
 
     @Query(value = """
