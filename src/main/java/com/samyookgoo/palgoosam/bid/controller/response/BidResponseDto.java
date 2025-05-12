79bd132ec7f4526b6d4af6a7099481178e95dd71
jaeyo03 <toyazx@naver.com>
2025-05-12 17:52:49 +0900

    feat: add bid response dto

---
+++ src/main/java/com/samyookgoo/palgoosam/bid/controller/response/BidResponseDto.java
@@ -0,0 +1,12 @@
+package com.samyookgoo.palgoosam.bid.controller.response;
+
+public class BidResponseDto {
+    private Boolean isWinning;
+    private Long bidPrice;
+    private Long bidId;
+    private Long userPrice;
+    private String title;
+    private String endTime;
+    private String startTime;
+    private String status;
+}
