fb84db7c4889c0f8ad3ba17a4ac651299d0a4c40
김우진 <ooojin523@gmail.com>
2025-05-12 16:50:40 +0900

    feat: repository 어노테이션 제거

--- src/main/java/com/samyookgoo/palgoosam/auction/repository/AuctionImageRepository.java
+++ src/main/java/com/samyookgoo/palgoosam/auction/repository/AuctionImageRepository.java
@@ -1,20 +1,9 @@
 package com.samyookgoo.palgoosam.auction.repository;
 
 import com.samyookgoo.palgoosam.auction.domain.AuctionImage;
-import jakarta.transaction.Transactional;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface AuctionImageRepository extends JpaRepository<AuctionImage, Long> {
     List<AuctionImage> findByAuctionId(Long auctionId);
-
-    @Transactional
-    @Modifying
-    @Query("DELETE FROM AuctionImage a WHERE a.auction.id = :auctionId")
-    void deleteByAuctionId(@Param("auctionId") Long auctionId);
 }
--- src/main/java/com/samyookgoo/palgoosam/auction/repository/CategoryRepository.java
+++ src/main/java/com/samyookgoo/palgoosam/auction/repository/CategoryRepository.java
@@ -2,8 +2,6 @@ package com.samyookgoo.palgoosam.auction.repository;
 
 import com.samyookgoo.palgoosam.auction.domain.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 }
