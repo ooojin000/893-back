46889391771bd6bb3ce69290dc3cd3484f6507c7
jaeyo03 <toyazx@naver.com>
2025-05-12 17:51:59 +0900

    feat: add user bid api

--- src/main/java/com/samyookgoo/palgoosam/user/controller/UserController.java
+++ src/main/java/com/samyookgoo/palgoosam/user/controller/UserController.java
@@ -1,5 +1,8 @@
 package com.samyookgoo.palgoosam.user.controller;
 
+import com.samyookgoo.palgoosam.bid.controller.response.BidResponseDto;
+import com.samyookgoo.palgoosam.bid.domain.Bid;
+import com.samyookgoo.palgoosam.bid.repository.BidRepository;
 import com.samyookgoo.palgoosam.user.domain.User;
 import com.samyookgoo.palgoosam.user.dto.UserInfoResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -7,11 +10,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.samyookgoo.palgoosam.auth.service.AuthService;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/users")
 @RequiredArgsConstructor
 public class UserController {
     private final AuthService authService;
+    private final BidRepository bidRepository;
 
     @CrossOrigin(origins="http://localhost:3000", allowCredentials="true")
     @GetMapping("/user-info")
@@ -26,5 +32,15 @@ public class UserController {
         );
 
         return ResponseEntity.ok(dto);
+    }
+
+    @CrossOrigin(origins="http://localhost:3000", allowCredentials="true")
+    @GetMapping("/bids")
+    public ResponseEntity<?> getUserInfoByEmail() {
+        User user = authService.getCurrentUser();
+
+        List<Bid> dtos = bidRepository.findAllByBidder_Id(user.getId());
+
+        return ResponseEntity.ok(dtos);
     }
 }
