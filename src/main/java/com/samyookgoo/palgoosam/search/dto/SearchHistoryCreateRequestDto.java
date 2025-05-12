27877ea80587bfe0629a56abe7b931c585557ff2
CodeVac513 <xssdsw@naver.com>
2025-05-12 11:16:46 +0900

    feat: dto 의 keyword속성에 대해 NotBlank 검증 추가

--- src/main/java/com/samyookgoo/palgoosam/search/dto/SearchHistoryCreateRequestDto.java
+++ src/main/java/com/samyookgoo/palgoosam/search/dto/SearchHistoryCreateRequestDto.java
@@ -1,10 +1,13 @@
 package com.samyookgoo.palgoosam.search.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class SearchHistoryCreateRequestDto {
+
+    @NotBlank
     String keyword;
 }
