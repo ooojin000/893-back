405e9ef083dd76e1f2e134c829a9eb3188c797e8
김우진 <ooojin523@gmail.com>
2025-05-12 17:01:20 +0900

    feat: 파일 처리 로직 수정

--- src/main/java/com/samyookgoo/palgoosam/auction/file/ResultFileStore.java
+++ src/main/java/com/samyookgoo/palgoosam/auction/file/ResultFileStore.java
@@ -1,32 +1,28 @@
 package com.samyookgoo.palgoosam.auction.file;
 
+import com.samyookgoo.palgoosam.auction.domain.Auction;
 import com.samyookgoo.palgoosam.auction.domain.AuctionImage;
-import java.io.File;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Data
 public class ResultFileStore {
-    private String folderPath;
-    private String storeFileName;
+    private String folderName;
     private String originalFileName;
+    private String storeFileName;
 
-
-    public String getFullPath() {
-        return folderPath + File.separator + storeFileName;
-    }
-
-    public static AuctionImage toEntity(ResultFileStore resultFileStore) {
+    public static AuctionImage toEntity(ResultFileStore file, Auction auction, int imageSeq) {
+        String imageUrl = "/uploads/" + file.getStoreFileName();
         return AuctionImage.builder()
-                .originalName(resultFileStore.getOriginalFileName())
-                .storeName(resultFileStore.getStoreFileName())
+                .auction(auction)
+                .originalName(file.getOriginalFileName())
+                .storeName(file.getStoreFileName())
+                .imageSeq(imageSeq)
+                .url(imageUrl)
                 .build();
     }
 
-    public boolean isVaild() {
-        return folderPath != null && storeFileName != null && originalFileName != null;
-    }
 }
