5f44424e542e89fc0eca4a01c67a6d6fc45976c3
CodeVac513 <xssdsw@naver.com>
2025-05-14 14:38:53 +0900

    feat: 알림 응답 DTO에 사용되는 엔티티 조회 로직 추가

--- src/main/java/com/samyookgoo/palgoosam/notification/repository/NotificationStatusRepository.java
+++ src/main/java/com/samyookgoo/palgoosam/notification/repository/NotificationStatusRepository.java
@@ -1,7 +1,16 @@
 package com.samyookgoo.palgoosam.notification.repository;
 
 import com.samyookgoo.palgoosam.notification.domain.NotificationStatus;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface NotificationStatusRepository extends JpaRepository<NotificationStatus, Long> {
+
+    @Query(value = """
+            SELECT * 
+            FROM notification_status
+            WHERE user_id = :userId AND is_deleted = false
+            """, nativeQuery = true)
+    List<NotificationStatus> findAllByUserId(Long userId);
 }
