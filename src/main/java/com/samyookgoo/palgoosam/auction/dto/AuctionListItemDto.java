0c15052e978649e958e535622459c8ad7475607f
jiyeonyh <wldus8827@gmail.com>
2025-05-14 09:40:27 +0900

    fix: resolve issue related to auction logic

--- src/main/java/com/samyookgoo/palgoosam/auction/dto/AuctionListItemDto.java
+++ src/main/java/com/samyookgoo/palgoosam/auction/dto/AuctionListItemDto.java
@@ -1,5 +1,6 @@
 package com.samyookgoo.palgoosam.auction.dto;
 
+import com.samyookgoo.palgoosam.auction.domain.AuctionStatus;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -19,7 +20,7 @@ public class AuctionListItemDto {
     private LocalDateTime startTime;
     private LocalDateTime endTime;
 
-    private String status;
+    private AuctionStatus status;
 
     private Integer basePrice;
     private Integer currentPrice;
