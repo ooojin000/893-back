c423ed81a639af6c8c338ebb23b315e0aa2c989d
김우진 <ooojin523@gmail.com>
2025-05-14 10:13:15 +0900

    feat: 스크랩 respository 수정

--- src/main/java/com/samyookgoo/palgoosam/user/repository/ScrapRepository.java
+++ src/main/java/com/samyookgoo/palgoosam/user/repository/ScrapRepository.java
@@ -1,10 +1,10 @@
 package com.samyookgoo.palgoosam.user.repository;
 
 import com.samyookgoo.palgoosam.auction.domain.Auction;
-import com.samyookgoo.palgoosam.user.domain.User;
-import java.util.Optional;
 import com.samyookgoo.palgoosam.user.domain.Scrap;
+import com.samyookgoo.palgoosam.user.domain.User;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -13,14 +13,16 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ScrapRepository extends JpaRepository<Scrap, Long> {
     int countByAuctionId(Long auctionId);
-  
+
     @Query(value = """
             SELECT *
             FROM scrap s
             WHERE s.auction_id IN (:auctionIdList)
             """, nativeQuery = true)
     List<Scrap> findByAuctionIdList(@Param("auctionIdList") List<Long> auctionIdList);
-    
+
+    boolean existsByUserIdAndAuctionId(Long userId, Long auctionId);
+
     boolean existsByUserAndAuction(User user, Auction auction);
 
     Optional<Scrap> findByUserAndAuction(User user, Auction auction);
