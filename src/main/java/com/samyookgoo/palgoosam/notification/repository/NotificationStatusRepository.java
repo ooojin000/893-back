d2153433a60668547840518bc608c3fd73f3aa86
CodeVac513 <xssdsw@naver.com>
2025-05-14 16:04:58 +0900

    feat: userId, notificationId를 통해 알림의 상태 조회

--- src/main/java/com/samyookgoo/palgoosam/notification/repository/NotificationStatusRepository.java
+++ src/main/java/com/samyookgoo/palgoosam/notification/repository/NotificationStatusRepository.java
@@ -2,6 +2,7 @@ package com.samyookgoo.palgoosam.notification.repository;
 
 import com.samyookgoo.palgoosam.notification.domain.NotificationStatus;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -13,4 +14,7 @@ public interface NotificationStatusRepository extends JpaRepository<Notification
             WHERE user_id = :userId AND is_deleted = false
             """, nativeQuery = true)
     List<NotificationStatus> findAllByUserIdAndIsDeletedFalse(Long userId);
+
+    Optional<NotificationStatus> findByUserIdAndNotificationHistoryId(Long userId, Long notificationHistoryId);
+
 }
