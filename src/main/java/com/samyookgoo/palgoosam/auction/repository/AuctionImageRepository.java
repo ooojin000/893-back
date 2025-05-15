38d5e7e62d89e3c86611aa8cbc98ae7a5ce6b419
CodeVac513 <xssdsw@naver.com>
2025-05-15 16:01:04 +0900

    fix: 입찰 내역 조회 시 이미지에서 NULL POINTER가 발생하는 버그 수정

--- src/main/java/com/samyookgoo/palgoosam/auction/repository/AuctionImageRepository.java
+++ src/main/java/com/samyookgoo/palgoosam/auction/repository/AuctionImageRepository.java
@@ -1,7 +1,6 @@
 package com.samyookgoo.palgoosam.auction.repository;
 
 import com.samyookgoo.palgoosam.auction.domain.AuctionImage;
-import com.samyookgoo.palgoosam.bid.projection.MainImageProjection;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -13,15 +12,6 @@ public interface AuctionImageRepository extends JpaRepository<AuctionImage, Long
 
     @Query("SELECT a FROM AuctionImage a WHERE a.auction.id IN :auctionIds AND a.imageSeq = 0")
     List<AuctionImage> findMainImagesByAuctionIds(@Param("auctionIds") List<Long> auctionIds);
-
-    @Query("""
-                SELECT ai.auction.id AS auctionId,
-                       ai.url          AS url
-                FROM AuctionImage ai
-                WHERE ai.auction.id IN :auctionIds
-                  AND ai.imageSeq = 0
-            """)
-    List<MainImageProjection> findPrjMainImagesByAuctionIds(@Param("auctionIds") List<Long> auctionIds);
 
     @Query("SELECT a FROM AuctionImage a " +
             "WHERE a.auction.id = :auctionId AND a.imageSeq = 0")
