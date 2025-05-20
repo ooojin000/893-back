1ce5e55690394c9f2de2a7df41333000273adab0
CodeVac513 <xssdsw@naver.com>
2025-05-20 09:49:34 +0900

    refactor: 중복된 ItemCondition 삭제 및 constant패키지로 위치 이동

--- src/main/java/com/samyookgoo/palgoosam/auction/constant/ItemCondition.java
+++ src/main/java/com/samyookgoo/palgoosam/auction/constant/ItemCondition.java
@@ -1,25 +1,9 @@
 package com.samyookgoo.palgoosam.auction.constant;
 
 public enum ItemCondition {
-    BRAND_NEW("brand_new"),
-    LIKE_NEW("like_new"),
-    GENTLY_USED("gently_used"),
-    HEAVILY_USED("heavily_used"),
-    DAMAGED("damaged");
-
-    private final String value;
-
-    ItemCondition(String value) {
-        this.value = value;
-    }
-
-    public String getValue() {
-        return value;
-    }
-
-    public static final String BRAND_NEW_VALUE = BRAND_NEW.getValue();
-    public static final String LIKE_NEW_VALUE = LIKE_NEW.getValue();
-    public static final String GENTLY_USED_VALUE = GENTLY_USED.getValue();
-    public static final String HEAVILY_USED_VALUE = HEAVILY_USED.getValue();
-    public static final String DAMAGED_VALUE = DAMAGED.getValue();
+    brand_new,
+    like_new,
+    gently_used,
+    heavily_used,
+    damaged
 }
