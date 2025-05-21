d376f5ad7ac6222202682ada8a16ac5b29616c46
CodeVac513 <xssdsw@naver.com>
2025-05-21 14:38:34 +0900

    docs: scrap 관련 스웨거 어노테이션 커스텀

--- src/main/java/com/samyookgoo/palgoosam/auction/controller/ScrapController.java
+++ src/main/java/com/samyookgoo/palgoosam/auction/controller/ScrapController.java
@@ -1,11 +1,10 @@
 package com.samyookgoo.palgoosam.auction.controller;
 
+import com.samyookgoo.palgoosam.auction.api_docs.scrap.AddScrapApi;
+import com.samyookgoo.palgoosam.auction.api_docs.scrap.RemoveScrapApi;
 import com.samyookgoo.palgoosam.auction.service.ScrapService;
 import com.samyookgoo.palgoosam.common.response.BaseResponse;
-import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -24,14 +23,7 @@ public class ScrapController {
 
     private final ScrapService scrapService;
 
-    @Operation(
-            summary = "스크랩 등록",
-            description = "특정 경매 상품을 스크랩합니다. 이미 스크랩된 상품일 경우 409(CONFLICT)를 반환합니다."
-    )
-    @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "스크랩 등록 성공"),
-            @ApiResponse(responseCode = "409", description = "이미 스크랩된 상품")
-    })
+    @AddScrapApi
     @PostMapping
     public ResponseEntity<BaseResponse<Void>> addScrap(
             @Parameter(name = "auctionId", description = "스크랩할 경매 상품의 ID", required = true)
@@ -47,14 +39,7 @@ public class ScrapController {
         }
     }
 
-    @Operation(
-            summary = "스크랩 취소",
-            description = "스크랩된 경매 상품을 취소합니다. 스크랩되지 않은 상품일 경우 409(CONFLICT)를 반환합니다."
-    )
-    @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "스크랩 취소 성공"),
-            @ApiResponse(responseCode = "409", description = "스크랩되지 않은 상품입니다.")
-    })
+    @RemoveScrapApi
     @DeleteMapping
     public ResponseEntity<BaseResponse<Void>> removeScrap(
             @Parameter(name = "auctionId", description = "스크랩 취소할 경매 상품의 ID", required = true)
