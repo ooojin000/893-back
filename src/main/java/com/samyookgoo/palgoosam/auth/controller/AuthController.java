e9419200b6a5c8822a19898f84dac1336dbd728a
CodeVac513 <xssdsw@naver.com>
2025-05-21 14:39:04 +0900

    docs: auth 관련 스웨거 어노테이션 커스텀

--- src/main/java/com/samyookgoo/palgoosam/auth/controller/AuthController.java
+++ src/main/java/com/samyookgoo/palgoosam/auth/controller/AuthController.java
@@ -1,14 +1,13 @@
 package com.samyookgoo.palgoosam.auth.controller;
 
 import com.samyookgoo.palgoosam.auth.JwtTokenProvider;
+import com.samyookgoo.palgoosam.auth.api_docs.LogoutApi;
+import com.samyookgoo.palgoosam.auth.api_docs.RefreshAccessTokenApi;
 import com.samyookgoo.palgoosam.auth.service.AuthService;
 import com.samyookgoo.palgoosam.user.domain.User;
 import com.samyookgoo.palgoosam.user.domain.UserJwtToken;
 import com.samyookgoo.palgoosam.user.repository.UserJwtTokenRepository;
 import io.jsonwebtoken.JwtException;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -37,14 +36,7 @@ public class AuthController {
     private final UserJwtTokenRepository userJwtTokenRepository;
     private final AuthService authService;
 
-    @Operation(
-            summary = "액세스 토큰 갱신",
-            description = "리프레시 토큰이 유효한 경우 새로운 액세스 토큰과 리프레시 토큰을 발급합니다."
-    )
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "토큰 재발급 성공"),
-            @ApiResponse(responseCode = "401", description = "리프레시 토큰이 유효하지 않거나 사용자 조회 실패")
-    })
+    @RefreshAccessTokenApi
     @PostMapping("/auth/refresh")
     public ResponseEntity<?> refreshAccessToken(
             @CookieValue("refreshToken") String refreshToken
@@ -103,14 +95,7 @@ public class AuthController {
                 .build();
     }
 
-    @Operation(
-            summary = "로그아웃",
-            description = "로그인한 사용자의 토큰을 만료시키고, 관련된 쿠키와 DB 정보를 삭제합니다."
-    )
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "로그아웃 성공"),
-            @ApiResponse(responseCode = "404", description = "사용자 정보를 찾을 수 없음")
-    })
+    @LogoutApi
     @Transactional
     @PostMapping("/auth/logout")
     public ResponseEntity<?> logout(HttpServletRequest req, HttpServletResponse res) {
