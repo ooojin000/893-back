2ca31541d3ad3a04e996bbe4b9203bfb7d5c5075
김우진 <ooojin523@gmail.com>
2025-05-27 17:36:46 +0900

    feat: 주문 정보 조회 책임을 PaymentService에서 OrderService로 이동

--- src/main/java/com/samyookgoo/palgoosam/payment/controller/OrderController.java
+++ src/main/java/com/samyookgoo/palgoosam/payment/controller/OrderController.java
@@ -4,11 +4,12 @@ import com.samyookgoo.palgoosam.auth.service.AuthService;
 import com.samyookgoo.palgoosam.common.response.BaseResponse;
 import com.samyookgoo.palgoosam.payment.api_docs.GetOrderPageApi;
 import com.samyookgoo.palgoosam.payment.controller.response.OrderResponse;
-import com.samyookgoo.palgoosam.payment.service.PaymentService;
+import com.samyookgoo.palgoosam.payment.service.OrderService;
 import com.samyookgoo.palgoosam.user.domain.User;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,12 +17,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/auctions")
 @Tag(name = "주문", description = "경매 상품 주문 정보 API")
 public class OrderController {
-    private final PaymentService paymentService;
+    private final OrderService orderService;
     private final AuthService authService;
 
     @GetOrderPageApi
@@ -34,7 +36,7 @@ public class OrderController {
         if (user == null) {
             throw new UsernameNotFoundException("유저를 찾을 수 없습니다.");
         }
-        OrderResponse response = paymentService.getOrder(auctionId, user.getId());
+        OrderResponse response = orderService.getOrder(auctionId, user.getId());
         return ResponseEntity.ok(BaseResponse.success("주문 정보 조회 성공", response)
         );
     }
