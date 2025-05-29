27ae95dc79c21b73222758cf46d0eac126fabd3a
jiyeonyh <wldus8827@gmail.com>
2025-05-29 16:24:02 +0900

    feat: migrate aws sdk from 1.x to 2.x version for S3 integration

--- src/main/java/com/samyookgoo/palgoosam/common/service/S3Service.java
+++ src/main/java/com/samyookgoo/palgoosam/common/service/S3Service.java
@@ -1,35 +1,43 @@
 package com.samyookgoo.palgoosam.common.service;
 
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.ObjectMetadata;
-import com.amazonaws.services.s3.model.S3Object;
-import com.amazonaws.services.s3.model.S3ObjectInputStream;
-import com.google.api.client.util.Value;
+import java.io.IOException;
 import java.io.InputStream;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 @Service
 @RequiredArgsConstructor
 public class S3Service {
-    private final AmazonS3 amazonS3;
+    private final S3Client s3Client;
 
     @Value("${cloud.aws.s3.bucket}")
     private String bucketName;
 
     // S3에 파일 업로드
-    public void uploadFile(String keyName, InputStream inputStream) {
-        amazonS3.putObject(bucketName, keyName, inputStream, new ObjectMetadata());
+    public void uploadFile(String keyName, MultipartFile file) throws IOException {
+        InputStream inputStream = file.getInputStream();
+
+        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                .bucket(bucketName)
+                .key(keyName)
+                .build();
+
+        s3Client.putObject(putObjectRequest, RequestBody.fromInputStream(inputStream, file.getSize()));
     }
 
-    // S3에서 파일 다운로드
-    public S3ObjectInputStream downloadFile(String keyName) {
-        S3Object s3Object = amazonS3.getObject(bucketName, keyName);
-        return s3Object.getObjectContent();
-    }
-
-    // S3에서 파일 삭제
+    // 파일 삭제
     public void deleteFile(String keyName) {
-        amazonS3.deleteObject(bucketName, keyName);
+        DeleteObjectRequest deleteObjectRequest = DeleteObjectRequest.builder()
+                .bucket(bucketName)
+                .key(keyName)
+                .build();
+
+        s3Client.deleteObject(deleteObjectRequest);
     }
 }
--- src/main/java/com/samyookgoo/palgoosam/config/AwsS3Config.java
+++ src/main/java/com/samyookgoo/palgoosam/config/AwsS3Config.java
@@ -1,20 +1,35 @@
 package com.samyookgoo.palgoosam.config;
 
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.google.api.client.util.Value;
+import java.net.URI;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
 public class AwsS3Config {
+    @Value("${cloud.aws.credentials.access-key}")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String secretKey;
+
     @Value("${cloud.aws.region.static}")
     private String region;
 
+    @Value("${cloud.aws.s3.endpoint-url}")
+    private String s3EndpointUrl;
+
     @Bean
-    public AmazonS3 amazonS3() {
-        return AmazonS3ClientBuilder.standard()
-                .withRegion(region)
+    public S3Client amazonS3() {
+        AwsBasicCredentials awsCredentials = AwsBasicCredentials.create(accessKey, secretKey);
+        return S3Client.builder()
+                .region(Region.of(region))
+                .endpointOverride(URI.create(s3EndpointUrl))
+                .credentialsProvider(StaticCredentialsProvider.create(awsCredentials))
                 .build();
     }
 }
