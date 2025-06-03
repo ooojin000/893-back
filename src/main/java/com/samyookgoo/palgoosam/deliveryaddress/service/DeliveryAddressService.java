d97f4a3e43e3c5a3c3e09896780093217a1dc90f
CodeVac513 <xssdsw@naver.com>
2025-06-03 23:38:04 +0900

    feat: update 최적화 및 기타 로직 리팩토링

--- src/main/java/com/samyookgoo/palgoosam/deliveryaddress/service/DeliveryAddressService.java
+++ src/main/java/com/samyookgoo/palgoosam/deliveryaddress/service/DeliveryAddressService.java
@@ -1,20 +1,20 @@
 package com.samyookgoo.palgoosam.deliveryaddress.service;
 
 import com.samyookgoo.palgoosam.auth.service.AuthService;
-import com.samyookgoo.palgoosam.common.response.BaseResponse;
 import com.samyookgoo.palgoosam.deliveryaddress.domain.DeliveryAddress;
 import com.samyookgoo.palgoosam.deliveryaddress.dto.DeliveryAddressRequestDto;
 import com.samyookgoo.palgoosam.deliveryaddress.dto.DeliveryAddressResponseDto;
+import com.samyookgoo.palgoosam.deliveryaddress.exception.DeliveryAddressNotFoundException;
 import com.samyookgoo.palgoosam.deliveryaddress.repository.DeliveryAddressRepository;
+import com.samyookgoo.palgoosam.global.exception.ErrorCode;
 import com.samyookgoo.palgoosam.user.domain.User;
+import com.samyookgoo.palgoosam.user.exception.UserForbiddenException;
+import com.samyookgoo.palgoosam.user.exception.UserUnauthorizedException;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -23,102 +23,55 @@ public class DeliveryAddressService {
     private final AuthService authService;
 
     public List<DeliveryAddressResponseDto> getUserDeliveryAddresses() {
-        User user = authService.getCurrentUser();
-        if (user == null) {
-            throw new UsernameNotFoundException("유저를 찾을 수 없습니다.");
-        }
-        List<DeliveryAddress> entities = getDeliveryAddressByUserId(user.getId());
+        User user = getAuthenticatedUser(authService.getCurrentUser());
+        List<DeliveryAddress> deliveryAddressList = deliveryAddressRepository.findAllByUser_Id(user.getId());
 
-        List<DeliveryAddressResponseDto> dtoList = entities.stream()
+        return deliveryAddressList.stream()
                 .map(DeliveryAddressResponseDto::of)
                 .collect(Collectors.toList());
-
-        return dtoList;
     }
 
-    public List<DeliveryAddress> getDeliveryAddressByUserId(Long userId) {
-        return deliveryAddressRepository.findAllByUser_Id(userId);
-    }
-
+    @Transactional
     public void deleteUserDeliveryAddress(Long deliveryAddressId) {
-        User user = authService.getCurrentUser();
-        if (user == null) {
-            throw new UsernameNotFoundException("유저를 찾을 수 없습니다.");
-        }
-        boolean isDeliveryAddressDeleteSuccess = deleteUserDeliveryAddress(user, deliveryAddressId);
+        User user = getAuthenticatedUser(authService.getCurrentUser());
+        DeliveryAddress deliveryAddress = deliveryAddressRepository.findByUserAndId(user, deliveryAddressId)
+                .orElseThrow(() -> new DeliveryAddressNotFoundException(ErrorCode.DELIVERY_ADDRESS_NOT_FOUND));
 
-        if (!isDeliveryAddressDeleteSuccess) {
-            ResponseEntity
-                    .status(HttpStatus.BAD_REQUEST)
-                    .body(BaseResponse.error("배송지 삭제 에러", null));
+        if (deliveryAddress.hasPermission(user.getId())) {
+            deliveryAddressRepository.deleteById(deliveryAddress.getId());
+        } else {
+            throw new UserForbiddenException();
         }
     }
 
-    public boolean modifyDefaultDeliveryAddress(User user, Long addressId) {
-        DeliveryAddress deliveryAddress = deliveryAddressRepository
-                .findByUserAndId(user, addressId)
-                .orElseThrow(() -> new ResponseStatusException(
-                        HttpStatus.NOT_FOUND, "변경할 주소를 찾을 수 없습니다."
-                ));
-
-        deliveryAddressRepository.unsetAllDefaults(user.getId());
-        deliveryAddressRepository.updateDefault(deliveryAddress.getId(), user.getId());
-
-        return true;
-    }
-
+    @Transactional
     public void postUserDeliveryAddress(DeliveryAddressRequestDto requestDto) {
-        User user = authService.getCurrentUser();
+        User user = getAuthenticatedUser(authService.getCurrentUser());
+        DeliveryAddress isDeliveryAddressSaveSuccess = DeliveryAddress.from(requestDto, user);
+        deliveryAddressRepository.save(isDeliveryAddressSaveSuccess);
+    }
+
+    @Transactional
+    public void modifyDefaultDeliveryAddress(Long deliveryAddressId) {
+        User user = getAuthenticatedUser(authService.getCurrentUser());
+        DeliveryAddress target = deliveryAddressRepository.findByUserAndIsDefaultTrue(user)
+                .orElseThrow(() -> new DeliveryAddressNotFoundException(ErrorCode.DELIVERY_ADDRESS_NOT_FOUND));
+
+        DeliveryAddress deliveryAddressToDefault = deliveryAddressRepository.findById(deliveryAddressId)
+                .orElseThrow(() -> new DeliveryAddressNotFoundException(ErrorCode.DELIVERY_ADDRESS_NOT_FOUND));
+
+        if (target.hasPermission(user.getId()) && deliveryAddressToDefault.hasPermission(user.getId())) {
+            target.removeDefault();
+            deliveryAddressToDefault.setDefault();
+        } else {
+            throw new UserForbiddenException();
+        }
+    }
+
+    public User getAuthenticatedUser(User user) {
         if (user == null) {
-            throw new UsernameNotFoundException("유저를 찾을 수 없습니다.");
+            throw new UserUnauthorizedException();
         }
-        DeliveryAddress entity = DeliveryAddress.builder()
-                .user(user)
-                .name(requestDto.getName())
-                .phoneNumber(requestDto.getPhoneNumber())
-                .zipCode(requestDto.getZipCode())
-                .addressLine1(requestDto.getAddressLine1())
-                .addressLine2(requestDto.getAddressLine2())
-                .isDefault(requestDto.isDefault())
-                .build();
-
-        boolean isDeliveryAddressSaveSuccess = saveUserDeliveryAddress(entity);
-
-        if (!isDeliveryAddressSaveSuccess) {
-            ResponseEntity
-                    .status(HttpStatus.BAD_REQUEST)
-                    .body(BaseResponse.error("배송지 등록 에러", null));
-        }
-    }
-
-    public boolean saveUserDeliveryAddress(DeliveryAddress deliveryAddress) {
-        DeliveryAddress saved = deliveryAddressRepository.save(deliveryAddress);
-        return true;
-    }
-
-    public boolean deleteUserDeliveryAddress(User user, Long addressId) {
-        DeliveryAddress deliveryAddress = deliveryAddressRepository
-                .findByUserAndId(user, addressId)
-                .orElseThrow(() -> new ResponseStatusException(
-                        HttpStatus.NOT_FOUND, "삭제할 주소를 찾을 수 없습니다."
-                ));
-
-        deliveryAddressRepository.deleteById(deliveryAddress.getId());
-
-        return true;
-    }
-
-    public void patchUserDefaultAddress(Long deliveryAddressId) {
-        User user = authService.getCurrentUser();
-        if (user == null) {
-            throw new UsernameNotFoundException("유저를 찾을 수 없습니다.");
-        }
-        boolean isModifySuccess = modifyDefaultDeliveryAddress(user, deliveryAddressId);
-
-        if (!isModifySuccess) {
-            ResponseEntity
-                    .status(HttpStatus.BAD_REQUEST)
-                    .body(BaseResponse.error("기본 배송지 변경 에러", null));
-        }
+        return user;
     }
 }
