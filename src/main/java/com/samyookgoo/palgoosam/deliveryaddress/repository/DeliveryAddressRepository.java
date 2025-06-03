ddc9029e7b153445d95652e2f2e87449fc6ff950
CodeVac513 <xssdsw@naver.com>
2025-06-03 23:38:51 +0900

    feat: update로직 삭제 후 entity에게 위임 및 조회 로직 추가

--- src/main/java/com/samyookgoo/palgoosam/deliveryaddress/repository/DeliveryAddressRepository.java
+++ src/main/java/com/samyookgoo/palgoosam/deliveryaddress/repository/DeliveryAddressRepository.java
@@ -5,33 +5,15 @@ import com.samyookgoo.palgoosam.user.domain.User;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.transaction.annotation.Transactional;
 
 public interface DeliveryAddressRepository extends JpaRepository<DeliveryAddress, Long> {
-    @Modifying(clearAutomatically = true)
-    @Transactional
-    @Query("""
-                UPDATE DeliveryAddress d
-                   SET d.isDefault = false
-                 WHERE d.user.id = :userId
-            """)
-    int unsetAllDefaults(@Param("userId") Long userId);
-
-    @Modifying(clearAutomatically = true)
-    @Transactional
-    @Query("""
-                UPDATE DeliveryAddress d
-                  SET d.isDefault = true
-                WHERE d.id = :id AND d.user.id = :userId
-            """)
-    int updateDefault(@Param("id") Long id, @Param("userId") Long userId);
-
     List<DeliveryAddress> findAllByUser_Id(Long userId);
 
     Optional<DeliveryAddress> findByUserAndId(User user, Long addressId);
 
     Optional<DeliveryAddress> findDefaultByUserId(Long userId);
+
+    Optional<DeliveryAddress> findByUserAndIsDefaultTrue(User user);
+
+    Optional<DeliveryAddress> findById(Long id);
 }
