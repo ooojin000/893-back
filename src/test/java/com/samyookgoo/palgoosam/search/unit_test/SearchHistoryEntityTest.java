411715017768d1773e0bd3b762cda8eb88e95912
CodeVac513 <xssdsw@naver.com>
2025-06-06 17:08:46 +0900

    test: 유효성 검증에 대한 예외 테스트 추가

--- src/test/java/com/samyookgoo/palgoosam/search/unit_test/SearchHistoryEntityTest.java
+++ src/test/java/com/samyookgoo/palgoosam/search/unit_test/SearchHistoryEntityTest.java
@@ -1,9 +1,14 @@
 package com.samyookgoo.palgoosam.search.unit_test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import com.samyookgoo.palgoosam.global.exception.ErrorCode;
 import com.samyookgoo.palgoosam.search.domain.SearchHistory;
+import com.samyookgoo.palgoosam.search.exception.SearchHistoryBadRequestException;
 import com.samyookgoo.palgoosam.user.domain.User;
+import com.samyookgoo.palgoosam.user.exception.UserForbiddenException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.test.context.ActiveProfiles;
@@ -14,7 +19,7 @@ class SearchHistoryEntityTest {
 
     @Test
     @DisplayName("검색 기록은 검색 카운트를 1회 증가시키고, 논리적인 삭제 상태를 복구할 수 있다.")
-    public void Given_SearchHistory_When_CallRestoreAndIncrementMethod_Then_UpdateIsDeleteAndSearchCount() {
+    public void restoreAndIncrement_DeletedRecord_RestoresAndIncrementsCount() {
         //given
         SearchHistory target = SearchHistory.builder()
                 .isDeleted(true)
@@ -34,8 +39,8 @@ class SearchHistoryEntityTest {
     }
 
     @Test
-    @DisplayName("검색 기록은 논리적인 삭제 상태로 변경할 수 있다.")
-    public void Given_SearchHistory_When_CallsoftDeleteSearchHistoryMethod_Then_UpdateIsDelete() {
+    @DisplayName("검색 기록은 논리 삭제를 할 수 있다.")
+    public void delete_ValidRecord_MarksAsDeleted() {
         //given
         SearchHistory target = SearchHistory.builder()
                 .isDeleted(false)
@@ -45,35 +50,15 @@ class SearchHistoryEntityTest {
                 .isDeleted(true)
                 .build();
         //when
-        target.softDeleteSearchHistory();
+        target.delete();
 
         //then
         assertThat(target.getIsDeleted()).isEqualTo(expected.getIsDeleted());
     }
 
     @Test
-    @DisplayName("유저가 검색 기록에 대한 권한이 없다면 참을 반환한다.")
-    public void Given_SearchHistoryAndUserId_When_CallhasPermissionMethod_Then_ReturnTrue() {
-        //given
-        User user = User.builder()
-                .id(1L)
-                .build();
-
-        SearchHistory target = SearchHistory.builder()
-                .isDeleted(false)
-                .user(user)
-                .build();
-
-        //when
-        Boolean result = target.hasPermission(user.getId());
-
-        //then
-        assertThat(result).isTrue();
-    }
-
-    @Test
-    @DisplayName("유저가 검색 기록에 대한 권한이 없다면 거짓을 반환한다.")
-    public void Given_SearchHistoryAndUserId_When_CallhasPermissionMethod_Then_ReturnFalse() {
+    @DisplayName("유저가 검색 권한에 대한 권한이 없다면 Forbidden 예외를 던진다.")
+    public void checkPermission_UnauthorizedUser_ThrowsForbiddenException() {
         //given
         User userWithPermission = User.builder()
                 .id(1L)
@@ -89,10 +74,67 @@ class SearchHistoryEntityTest {
                 .build();
 
         //when
-        Boolean result = target.hasPermission(userWithoutPermission.getId());
+        UserForbiddenException userForbiddenException = assertThrows(UserForbiddenException.class,
+                () -> target.checkPermission(userWithoutPermission.getId()));
 
         //then
-        assertThat(result).isFalse();
+        assertThat(userForbiddenException.getErrorCode()).isEqualTo(ErrorCode.FORBIDDEN);
+    }
+
+    @Test
+    @DisplayName("유저가 검색 권한에 대한 권한이 있다면 아무 동작을 하지 않는다.")
+    public void checkPermission_AuthorizedUser_DoesNotThrow() {
+        //given
+        User userWithPermission = User.builder()
+                .id(1L)
+                .build();
+
+        SearchHistory target = SearchHistory.builder()
+                .isDeleted(false)
+                .user(userWithPermission)
+                .build();
+
+        //when & then
+        assertThatCode(() -> target.checkPermission(userWithPermission.getId()))
+                .doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("검색 기록은 삭제할 수 있다면 아무 동작을 하지 않는다.")
+    public void checkDeletable_isDeletedFalse_DoesNotThrow() {
+        //given
+        User user = User.builder()
+                .id(1L)
+                .build();
+
+        SearchHistory target = SearchHistory.builder()
+                .isDeleted(false)
+                .user(user)
+                .build();
+
+        //when & then
+        assertThatCode(target::checkDeletable).doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("검색 기록은 이미 삭제되었다면 삭제 시 Bad Request 예외를 던진다.")
+    public void checkDeletable_isDeletedTrue_ThrowsBadRequestException() {
+        //given
+        User user = User.builder()
+                .id(1L)
+                .build();
+
+        SearchHistory target = SearchHistory.builder()
+                .isDeleted(true)
+                .user(user)
+                .build();
+
+        //when
+        SearchHistoryBadRequestException badRequestException = assertThrows(SearchHistoryBadRequestException.class,
+                target::checkDeletable);
+
+        //then
+        assertThat(badRequestException.getErrorCode()).isEqualTo(ErrorCode.SEARCH_HISTORY_ALREADY_DELETED_BAD_REQUEST);
     }
 
 }
