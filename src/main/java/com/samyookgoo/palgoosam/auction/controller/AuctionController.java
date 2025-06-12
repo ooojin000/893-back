83f74763260edc5de9364ce0e7b3c6cff1d9cbc1
김우진 <ooojin523@gmail.com>
2025-06-12 14:04:44 +0900

    feat: requestBody로 수정

--- src/main/java/com/samyookgoo/palgoosam/auction/controller/AuctionController.java
+++ src/main/java/com/samyookgoo/palgoosam/auction/controller/AuctionController.java
@@ -30,6 +30,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
@@ -53,7 +54,7 @@ public class AuctionController {
     @AuctionCreateApi
     @PostMapping
     public ResponseEntity<BaseResponse<AuctionCreateResponse>> createAuction(
-            @RequestPart("request") @Valid AuctionCreateRequest request) {
+            @RequestBody @Valid AuctionCreateRequest request) {
 
         AuctionCreateResponse response = auctionService.createAuction(request);
         return ResponseEntity.status(HttpStatus.CREATED)
