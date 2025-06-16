0633303a4610348dbc0fd06078ca06d964daf50a
김우진 <ooojin523@gmail.com>
2025-06-16 09:54:07 +0900

    feat: 반환값 dto단으로 분리

--- src/main/java/com/samyookgoo/palgoosam/auction/dto/home/UpcomingAuctionResponse.java
+++ src/main/java/com/samyookgoo/palgoosam/auction/dto/home/UpcomingAuctionResponse.java
@@ -1,6 +1,7 @@
 package com.samyookgoo.palgoosam.auction.dto.home;
 
 import com.samyookgoo.palgoosam.auction.constant.ItemCondition;
+import com.samyookgoo.palgoosam.auction.projection.UpcomingAuction;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -15,4 +16,17 @@ public class UpcomingAuctionResponse {
     private Integer scrapCount;
     private String thumbnailUrl;
     private String leftTime;
+
+    public static UpcomingAuctionResponse from(UpcomingAuction u, int scrapCount, String leftTime) {
+        return UpcomingAuctionResponse.builder()
+                .auctionId(u.getAuctionId())
+                .title(u.getTitle())
+                .description(u.getDescription())
+                .itemCondition(u.getItemCondition())
+                .basePrice(u.getBasePrice())
+                .scrapCount(scrapCount)
+                .thumbnailUrl(u.getThumbnailUrl())
+                .leftTime(leftTime)
+                .build();
+    }
 }
