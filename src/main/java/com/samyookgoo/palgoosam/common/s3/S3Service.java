90ba925be64a59560372506922cc32ed71acc047
김우진 <ooojin523@gmail.com>
2025-06-18 12:57:24 +0900

    feat: s3Service 로직 수정

--- src/main/java/com/samyookgoo/palgoosam/common/s3/S3Service.java
+++ src/main/java/com/samyookgoo/palgoosam/common/s3/S3Service.java
@@ -10,12 +10,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
-import software.amazon.awssdk.services.s3.model.GetObjectRequest;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.s3.model.S3Exception;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
-import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
-import software.amazon.awssdk.services.s3.presigner.model.PresignedGetObjectRequest;
 import software.amazon.awssdk.services.s3.presigner.model.PresignedPutObjectRequest;
 import software.amazon.awssdk.services.s3.presigner.model.PutObjectPresignRequest;
 
@@ -32,14 +29,13 @@ public class S3Service {
     @Value("${cloud.aws.region.static}")
     private String region;
 
-    @Value("${cloud.aws.s3.presign-duration-minutes:30}")
-    private long presignDurationMinutes;
-
     public S3PresignedResponse createPresignedUrl(S3PresignedRequest request) {
         try {
             String originalName = request.getFileName();
             String extension = originalName.substring(originalName.lastIndexOf("."));
             String storeFileName = UUID.randomUUID() + extension;
+
+            String publicUrl = "https://" + bucket + ".s3." + region + ".amazonaws.com/" + storeFileName;
 
             PutObjectRequest putObjectRequest = PutObjectRequest.builder()
                     .bucket(bucket)
@@ -49,7 +45,7 @@ public class S3Service {
 
             // Presigned URL 생성
             PutObjectPresignRequest presignRequest = PutObjectPresignRequest.builder()
-                    .signatureDuration(Duration.ofMinutes(presignDurationMinutes)) // yml 60분 유효
+                    .signatureDuration(Duration.ofMinutes(10))
                     .putObjectRequest(putObjectRequest)
                     .build();
 
@@ -58,36 +54,11 @@ public class S3Service {
             return S3PresignedResponse.builder()
                     .presignedUrl(presignedRequest.url().toString())
                     .storeName(storeFileName)
-                    .expirationTime(presignedRequest.expiration())
+                    .publicUrl(publicUrl)
                     .build();
 
         } catch (S3Exception e) {
             throw new RuntimeException("S3 URL 생성 중 오류가 발생했습니다.", e);
-        }
-    }
-
-    public S3PresignedResponse getPresignedUrl(String storeName) {
-        try {
-            GetObjectRequest getObjectRequest = GetObjectRequest.builder()
-                    .bucket(bucket)
-                    .key(storeName)
-                    .build();
-
-            GetObjectPresignRequest presignRequest = GetObjectPresignRequest.builder()
-                    .signatureDuration(Duration.ofMinutes(presignDurationMinutes))
-                    .getObjectRequest(getObjectRequest)
-                    .build();
-
-            PresignedGetObjectRequest presignedRequest = s3Presigner.presignGetObject(presignRequest);
-
-            return S3PresignedResponse.builder()
-                    .presignedUrl(presignedRequest.url().toString())
-                    .storeName(storeName)
-                    .expirationTime(presignedRequest.expiration())
-                    .build();
-
-        } catch (S3Exception e) {
-            throw new RuntimeException("S3 GET URL 생성 중 오류가 발생했습니다.", e);
         }
     }
 
